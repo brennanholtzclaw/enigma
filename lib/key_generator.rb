f1992f4b0dc6203b18d09e96cac77093cb732c35
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2015-12-13 11:41:15 -0700

    This was an aliased commit.

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -4,14 +4,16 @@ class KeyGenerator
   attr_reader :key
 
   def initialize(key = rand(99999))
-    @key = key
-    key_checker
+    @key = key.to_s
+    key_checker(@key)
   end
 
-  def key_checker
-    # binding.pry
+  def key_checker(key)
+    @key = ("%05d" % key) if @key.length < 5
     "Key length too long" if @key.to_s.length > 5
   end
+
+  
 
 
 end
--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -7,14 +7,21 @@ class KeyGeneratorTest < Minitest::Test
   def test_can_pass_in_own_key
     kg = KeyGenerator.new(12345)
 
-    assert_equal 12345, kg.key
+    assert_equal "12345", kg.key
   end
 
   def test_key_generator_rejects_keys_longer_than_five_chars
     kg = KeyGenerator.new(123456)
 
-    assert_equal "Key length too long", kg.key_checker
+    assert_equal "Key length too long", kg.key_checker(kg.key)
   end
+
+  def test_key_checker_fixes_a_key_less_than_five_characters
+    kg = KeyGenerator.new(1234)
+
+    assert_equal "01234", kg.key
+  end
+
 
 end
 
