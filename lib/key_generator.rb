982239661cadc1f262e4e4be954591657e2df372
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2015-12-13 10:48:27 -0700

    key generator is random and should print a message if key is too long - should implement a 'raise exception' in that edge case

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,9 +1,16 @@
 require 'pry'
 
 class KeyGenerator
+  attr_reader :key
 
+  def initialize(key = rand(99999))
+    @key = key
+    key_checker
+  end
 
-  def initialize
+  def key_checker
+    # binding.pry
+    "Key length too long" if @key.to_s.length > 5
   end
 
 
--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -4,5 +4,19 @@ require './lib/key_generator'
 
 class KeyGeneratorTest < Minitest::Test
 
+  def test_can_pass_in_own_key
+    kg = KeyGenerator.new(12345)
+
+    assert_equal 12345, kg.key
+  end
+
+  def test_key_generator_rejects_keys_longer_than_five_chars
+    kg = KeyGenerator.new(123456)
+
+    assert_equal "Key length too long", kg.key_checker
+  end
 
 end
+
+
+# @key = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
