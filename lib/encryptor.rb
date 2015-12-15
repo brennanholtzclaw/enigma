2716034de179569671949ad970281cfc71b7969d
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2015-12-14 17:04:57 -0700

    moving into breaking down the message and encrypting more than one letter - about to try a crazy idea, hence the commit.

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -5,18 +5,19 @@ require './lib/char_map'
 require './lib/offset_calculator'
 
 class Encryptor
-  attr_reader :characters, :key, :date, :message, :offsets, :a_offset
+  attr_reader :characters, :key, :date, :message, :offsets,
+              :a_offset, :b_offset, :c_offset, :d_offset
 
   def initialize(message = nil, key = rand(99999), date = Date.today)
     @key = KeyGenerator.new(key)
     @characters = CharMap.new.characters
     @date = date_format(date)
-    # @message = encrypt(message)
-    @offsets = OffsetCalculator.new(@key, date)
+    @offsets = OffsetCalculator.new(key, date)
     @a_offset = @offsets.a_final_off
     @b_offset = @offsets.b_final_off
     @c_offset = @offsets.c_final_off
     @d_offset = @offsets.d_final_off
+    # @message = encrypt(message)
   end
 
   def date_format(date)
@@ -29,12 +30,70 @@ class Encryptor
 
   def encrypt(message)
     # 39
-    # binding.pry
-    message.split.each do |char|
-      @characters[(@a_offset - 1)]
-    end.join
+    breakdown = message.chars
+
+    a_letters = []
+    b_letters = []
+    c_letters = []
+    d_letters = []
+
+    breakdown.each do |letter|
+     if breakdown.index(letter) % 4 == 0 || breakdown.index(letter) == 0
+       a_letters << letter
+     elsif breakdown.index(letter) % 4 == 1 || breakdown.index(letter) == 1
+       b_letters << letter
+     elsif breakdown.index(letter) % 4 == 2 || breakdown.index(letter) == 2
+       c_letters << letter
+     elsif breakdown.index(letter) % 4 == 3 || breakdown.index(letter) == 3
+       d_letters << i
+     end
+    #  binding.pry
+   end
+
+    a_rotate(a_letters).join if !a_letters.empty?
+    b_rotate(b_letters) if !b_letters.empty?
+    c_rotate(c_letters) if !c_letters.empty?
+    d_rotate(d_letters) if !d_letters.empty?
 
   end
 
+  def a_rotate(input)
+    a_encrypted = input.map do |char|
+      i = @characters.index(char)
+      @characters[(i + @a_offset)]
+    end
+    binding.pry
+    a_encrypted
+  end
+
+  def rotate
+
+
+    # message_indexes = message.chars.map do |char|
+    #   @characters.index(char)
+    # end
+    #
+    # encrypted_message = message.chars.map do |char|
+    #   @characters[@a_offset]
+    # end
+
+    # encrypted_message.join
 
 end
+#
+# 'HOW TO ENCRYPT A MESSAGE'
+# "Take the incoming message and break it into it's characters, including spaces
+# and some simple punctuation. As those pieces pass through the encryptor box
+# the first one will be rotated by the 'A' Offset, the second by the 'B', the
+# third by the 'C', and the fourth by the 'D'. After that the process should
+# cycle through until the end of the message.
+#
+# You could iterate through the message array, constantly checking the index value of
+
+
+"IN THE ENCRYPT METHOD ABOVE!
+take the breakdown and add the encryption into the 'each do' block...
+ie: if breakdown.index(letter) % 4 == 0 || breakdown.index(letter) == 0
+      @characters[(@characters.index(letter) + @a_offset)]
+
+that should end you up with an array of the encrypted letters? maybe?"
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -1,3 +1,4 @@
+require 'date'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/encryptor'
@@ -14,6 +15,7 @@ class EncryptorTest < Minitest::Test
   end
 
   def test_you_can_pass_a_message_to_the_encryptor
+    skip # - changed implementation of message after this test
     e = Encryptor.new("message")
 
     assert_equal "message", e.message
@@ -32,24 +34,50 @@ class EncryptorTest < Minitest::Test
     e = Encryptor.new("message", 12345)
     f = Encryptor.new("message", 12345, 160886)
 
-    assert_equal "131215", e.date
+    t = Date.today
+    today = t.strftime("%d") + t.strftime("%m") + t.strftime("%y")
+
+    assert_equal today, e.date
     assert_equal "160886", f.date
     refute_equal "069420", e.date
     refute_equal "lollol", f.date
   end
 
   def test_it_forms_offsets
-    e = Encryptor.new("message", 12345, 160886)
-# binding.pry
-    assert_equal 0, e.a_offset
-    assert_equal 0, e.b_offset
-    assert_equal 0, e.c_offset
-    assert_equal 0, e.d_offset
-  end
-  # def test_encrypts_a_single_letter_string
-  #   e = Encryptor.new("message", 12345, 131215)
-  #
-  #   assert_equal "s", e.encrypt("a")
-  # end
+    e = Encryptor.new("message", "12345", "131215")
 
+    assert_equal 18, e.a_offset
+    assert_equal 25, e.b_offset
+    assert_equal 36, e.c_offset
+    assert_equal 50, e.d_offset
+  end
+
+  def test_encrypts_a_single_letter_string
+    e = Encryptor.new("message", 12345, 131215)
+
+    assert_equal "s", e.encrypt("a")
+  end
+
+  def test_encrypts_two_letters
+    e = Encryptor.new("message", 12345, 131215)
+
+    assert_equal "s0", e.encrypt("ab")
+  end
+
+end
+
+a = "this is a message"
+
+b = a.chars
+
+a_array = []
+b_array = []
+c_array = []
+d_array = []
+
+b.each do |w, x, y, z|
+  a_array << w
+  b_array << x
+  c_array << y
+  d_array << z
 end
